82052f020c49b01de5049aa50c6af2cf2a5056f4
Mario <mario.mario@y7mail.com>
2024-08-16 15:06:53 +1000

    More clever health rounding

--- common/src/main/java/vice/sol_valheim/mixin/PlayerEntityMixin.java
+++ common/src/main/java/vice/sol_valheim/mixin/PlayerEntityMixin.java
@@ -99,8 +99,8 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerEn
         }
 
         float maxhp = Math.min(40, (SOLValheim.Config.common.startingHealth * 2) + sol_valheim$food_data.getTotalFoodNutrition());
-        if (((int)maxhp % 2) != 0)
-            maxhp += 1;
+        // hack: round to full hearts
+        maxhp = Math.round(maxhp / 2) * 2;
 
         Player player = (Player) (LivingEntity) this;
         player.getFoodData().setSaturation(0);
